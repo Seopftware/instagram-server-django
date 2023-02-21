ed3ba6a87d3cc5701f21d9597458d7305e4dc88d
INSEOP KIM <inseop0813@gmail.com>
2023-02-21 09:50:25 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ AUTH_USER_MODEL = "users.User"
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = env("SECRET_KEY")
-print(env("SECRET_KEY"))
+# print(env("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
